85deb6c0eb96dd1e406ed8e0c58facb1d3c9f172
JuliRed <julianaredondo15@gmail.com>
2021-02-19 17:21:59 -0500

    Added Grid for movies

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Juliana Redondo Ferrell on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
